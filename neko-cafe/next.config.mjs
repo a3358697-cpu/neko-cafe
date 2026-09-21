79eed8bd1531077e93c34b89d03eec8856aef5eb
a3358697-cpu <a3358697@gmail.com>
2026-09-21 13:19:31 +0800

    Add files via upload

--- neko-cafe/next.config.mjs
+++ neko-cafe/next.config.mjs
@@ -1,11 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
-  },
+  output: 'export',
   images: {
     unoptimized: true,
   },
-}
+  basePath: process.env.NODE_ENV === 'production' ? '/你的repo名稱' : '',
+};
 
-export default nextConfig
+export default nextConfig;
